b7c122e387d8c973c2de7728102b9736f04047ae
chanhduypq <chanhduypq@gmail.com>
2017-10-27 00:24:26 +0700

    no message

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'mysql_user';
-$db['default']['password'] = 'password';
-$db['default']['database'] = 'database';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'code';
 $db['default']['port']     = '3306';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'bf_';
